9f216ae1d5d2abe2f2c02b9d7ba8e909f6986979
wira <wira08@live.se>
2020-05-22 20:47:49 +0200

    adding new css file for admin and product page

--- public/admin/layout/header.php
+++ public/admin/layout/header.php
@@ -5,6 +5,8 @@
   
   <title><?php echo $pageTitle; ?></title>
 
+  <link rel="stylesheet" type="text/css" href="css/admin_page.css"/>
+  <link rel="stylesheet" type="text/css" href="css/products_page.css"/>
   <link rel="stylesheet" type="text/css" href="css/forms.css"/>
   <link rel="stylesheet" type="text/css" href="css/style.css"/>
 </head>
