743914461987564f1bb16630cd5eeb0c665becbc
wira <wira08@live.se>
2020-05-28 18:28:45 +0200

    added product cards to front page

--- public/index.php
+++ public/index.php
@@ -6,7 +6,7 @@ $pageId = 'home';
 
 checkSuccessLogin();
 // $posts = fetchAllPosts();
-
+$products = fetchAllProducts();
 ?>
 <?php include('layout/header.php'); ?>
 <!-- Sidans/Dokumentets huvudsakliga innehåll -->
@@ -14,7 +14,41 @@ checkSuccessLogin();
 	<?= $errorMsg ?>
 	<article class="border">
 		<h1>All Product</h1>
+
+
+		<? foreach ($products as $key => $product) { ?>
+			<!-- <div class="card-deck"> -->
+				<div class="card" style="width: 18rem; float: left;">
+					<img class="card-img-top" src="img/dummy-profile.png" alt="Card image cap">
+					<div class="card-body">
+						<h5 class="card-title"><?=htmlentities($product['title'])?></h5>
+						<p class="card-text"><?=htmlentities($product['price'])?> Kr</p>
+						<a href="#" class="btn btn-primary">Go somewhere</a>
+					</div>
+				</div>
+<!-- 
+				<div class="card" style="width: 18rem;">
+					<img class="card-img-top" src="img/dummy-profile.png" alt="Card image cap">
+					<div class="card-body">
+						<h5 class="card-title">Card title</h5>
+						<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+						<a href="#" class="btn btn-primary">Go somewhere</a>
+					</div>
+				</div>
+
+				<div class="card" style="width: 18rem;">
+					<img class="card-img-top" src="img/dummy-profile.png" alt="Card image cap">
+					<div class="card-body">
+						<h5 class="card-title">Card title</h5>
+						<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+						<a href="#" class="btn btn-primary">Go somewhere</a>
+					</div>
+				</div> -->
+			<!-- </div> -->
+		<? } ?>
+
 	</article>
+
 	
 </div>
 
