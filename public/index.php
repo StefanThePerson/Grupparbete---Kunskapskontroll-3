8096d1ed497554506641247195eb685af2385740
wira <wira08@live.se>
2020-05-28 19:30:02 +0200

    added single product page

--- public/index.php
+++ public/index.php
@@ -1,8 +1,8 @@
 <?php
 require('../src/config.php');
 require(SRC_PATH . 'dbconnect.php');
-$pageTitle = 'Home';
-$pageId = 'home';
+$pageTitle = 'Product';
+$pageId = 'singleproduct';
 
 checkSuccessLogin();
 // $posts = fetchAllPosts();
@@ -15,41 +15,25 @@ $products = fetchAllProducts();
 	<article class="border">
 		<h1>All Product</h1>
 
-
 		<? foreach ($products as $key => $product) { ?>
-			<!-- <div class="card-deck"> -->
-				<div class="card" style="width: 18rem; float: left;">
-					<img class="card-img-top" src="img/dummy-profile.png" alt="Card image cap">
-					<div class="card-body">
-						<h5 class="card-title"><?=htmlentities($product['title'])?></h5>
-						<p class="card-text"><?=htmlentities($product['price'])?> Kr</p>
-						<a href="#" class="btn btn-primary">Go somewhere</a>
-					</div>
-				</div>
-<!-- 
-				<div class="card" style="width: 18rem;">
-					<img class="card-img-top" src="img/dummy-profile.png" alt="Card image cap">
-					<div class="card-body">
-						<h5 class="card-title">Card title</h5>
-						<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-						<a href="#" class="btn btn-primary">Go somewhere</a>
-					</div>
-				</div>
+			<div class="card" style="width: 18rem; float: left;">
+				<img class="card-img-top" src="img/dummy-profile.png" alt="Card image cap">
+				<div class="card-body">
+					<h5 class="card-title"><?=htmlentities($product['title'])?></h5>
+					<p class="card-text"><?=htmlentities($product['price'])?> Kr</p>
+					<img src="<?=htmlentities($product['img_url'])?>">
 
-				<div class="card" style="width: 18rem;">
-					<img class="card-img-top" src="img/dummy-profile.png" alt="Card image cap">
-					<div class="card-body">
-						<h5 class="card-title">Card title</h5>
-						<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-						<a href="#" class="btn btn-primary">Go somewhere</a>
-					</div>
-				</div> -->
-			<!-- </div> -->
+					<form action="single_product.php" method="get">
+						<input type="hidden" name="id" value="<?=$product['id']?>">
+						<input type="submit" class="btn btn-primary" value="Product Details">
+					</form>
+
+					<!-- <a href="#" class="btn btn-primary">Go somewhere</a> -->
+				</div>
+			</div>
 		<? } ?>
-
-	</article>
-
 	
+	</article>
 </div>
 
 <?php include('layout/footer.php'); ?>
---
+++ public/single_product.php
@@ -0,0 +1,58 @@
+<?php
+require('../src/config.php');
+require(SRC_PATH . 'dbconnect.php');
+$pageTitle = 'Home';
+$pageId = 'home';
+
+// if (!isset($_GET['id']) || !is_numeric($_GET['id'])) {
+//   redirect('index.php?invalidPost');
+// }
+
+
+$product = fetchProductById($_GET['id']);
+
+?>
+<?php include('layout/header.php'); ?>
+<!-- Sidans/Dokumentets huvudsakliga innehåll -->
+<div id="content">
+	<!-- <?= $errorMsg ?> -->
+	<article class="border">
+		<h1>Product</h1>
+
+		<figure class="left top">
+			<img src="<?=htmlentities($product['img_url'])?>">
+			<!-- <img src="img/dummy-profile.png" width=130> -->
+			<figcaption>
+				<p>Exempel på bild med text</p>
+			</figcaption>
+		</figure>
+
+		<div class="profile-container">
+
+			<div>
+				<h3 class="profile-header">Product name: </h3>
+				<h3 class="profile-text" name="title"><?=htmlentities($product['title'])?></h3>
+			</div>
+
+			<div>            
+				<h3 class="profile-header">Price: </h3>
+				<h3 class="profile-text" name="price"><?=htmlentities($product['price'])?> Kr</h3>
+			</div>
+
+			<div>
+				<h3 class="profile-header">Product description: </h3>
+				<h3 class="profile-text" name="description"><?=htmlentities($product['description'])?></h3>
+			</div>
+
+			<br>
+			<form action="edit_profile.php" method="get" style="padding-bottom: 10px;">
+				<input type="hidden" name="id" value="<?=$user['id']?>">
+				<input type="submit" value="Add to Cart">
+			</form>
+		</div>
+
+	</article>
+</div>
+
+
+<?php include('layout/footer.php'); ?>
